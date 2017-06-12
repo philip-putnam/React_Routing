d2c808d29578652405eab1300456b411c3048d8c
Philip Putnam <putnamp989@gmail.com>
2017-06-11 19:17:52 -0700

    import NavLink and links to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
     <span className="icn-logo"><i className="material-icons">code</i></span>
     <ul className="main-nav">
-      <li><a href="/">Home</a></li>
-      <li><a href="/about">About</a></li>
-      <li><a href="/teachers">Teachers</a></li>
-      <li><a href="/courses">Courses</a></li>
+      <li><NavLink exact to="/">Home</NavLink></li>
+      <li><NavLink to="/about">About</NavLink></li>
+      <li><NavLink to="/teachers">Teachers</NavLink></li>
+      <li><NavLink to="/courses">Courses</NavLink></li>
     </ul>
   </header>
 );
