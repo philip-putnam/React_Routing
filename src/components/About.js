3fe2d2387fbad863fc3cfa0b1e75da4d404f8b85
Philip Putnam <putnamp989@gmail.com>
2017-06-11 19:08:40 -0700

    use inline render in App.js with the About route to pass a prop through to the component

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const About = () => (
+const About = (props) => (
   <div className="main-content">
-    <h2>About</h2>
+    <h2>{ props.title }</h2>
     <p>The front end course directory lists many of the courses we teach on HTML, CSS, JavaScript and more! Be sure to visit the Teachers section to view a list of our talented teachers. Or visit the Courses section and select a topic -- HTML, CSS, or JavaScript -- to see a list of our courses.</p>
   </div>
 );
--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ const App = () => (
     <div className='container'>
       <Header />
       <Route exact path="/" component={Home} />
-      <Route path='/about' component={About} />
+      <Route path='/about' render={ () => <About title="About" /> } />
       <Route path='/teachers' component={Teachers} />
       <Route path='/courses' component={Courses} />
     </div>
