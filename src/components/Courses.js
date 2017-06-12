3dd7b9692c34e61cd86f613a1eb42a663cb55fd0
Philip Putnam <putnamp989@gmail.com>
2017-06-11 19:41:52 -0700

    add imports and Routes on Courses.js /courses route

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
+
+// App components
+import CSS from './courses/CSS';
+import HTML from './courses/HTML';
+import JavaScript from './courses/JavaScript';
 
 const Courses = () => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2>
       <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+        <li><NavLink to='/courses/html'>HTML</NavLink></li>
+        <li><NavLink to='/courses/css'>CSS</NavLink></li>
+        <li><NavLink to='/courses/javascript'>JavaScript</NavLink></li>
       </ul>
     </div>
-    
+
+    <Route path='/courses/html' component={HTML} />
+    <Route path='/courses/css' component={CSS} />
+    <Route path='/courses/javascript' component={JavaScript} />
   </div>
 );
 
