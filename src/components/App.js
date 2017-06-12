9efece13b1bc8f802a58b1d2f99a1cc0f1a5a712
Philip Putnam <putnamp989@gmail.com>
2017-06-11 18:44:36 -0700

    add About & Home components with an exact path for the root route and an about route

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// App components
+import Home from './Home';
+import About from './About';
 
 const App = () => (
-  <div className='container'>
-
-  </div>
+  <BrowserRouter>
+    <div className='container'>
+      <Route exact path="/" component={Home} />
+      <Route path='/about' component={About} />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
