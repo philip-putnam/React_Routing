e48d4cf7b15afb4cc9bb31ca5d4d084b000a8de4
Philip Putnam <putnamp989@gmail.com>
2017-06-11 20:07:28 -0700

    add NotFound.js component & Switch import with link in App.js for 'page not found' to display on routes with no content

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // App components
 import Home from './Home';
@@ -7,15 +7,19 @@ import About from './About';
 import Header from './Header';
 import Teachers from './Teachers';
 import Courses from './Courses';
+import NotFound from './NotFound';
 
 const App = () => (
   <BrowserRouter>
     <div className='container'>
       <Header />
-      <Route exact path="/" component={Home} />
-      <Route path='/about' render={ () => <About title="About" /> } />
-      <Route path='/teachers' component={Teachers} />
-      <Route path='/courses' component={Courses} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path='/about' render={ () => <About title="About" /> } />
+        <Route path='/teachers' component={Teachers} />
+        <Route path='/courses' component={Courses} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
