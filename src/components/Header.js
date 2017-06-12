c28f3ccf5ccbdb5757a0b2eea3f2bb7ef5798418
Philip Putnam <putnamp989@gmail.com>
2017-06-11 19:23:33 -0700

    add google api cdn link for fonts to index.html, activeStyle and activeClassName inline attributes in Header.js for practice/reference

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ const Header = () => (
   <header>
     <span className="icn-logo"><i className="material-icons">code</i></span>
     <ul className="main-nav">
-      <li><NavLink exact to="/">Home</NavLink></li>
-      <li><NavLink to="/about">About</NavLink></li>
+      <li><NavLink exact to="/" activeStyle={{ background: 'tomato' }}>Home</NavLink></li>
+      <li><NavLink to="/about" activeClassName='actyMcActiveFace'>About</NavLink></li>
       <li><NavLink to="/teachers">Teachers</NavLink></li>
       <li><NavLink to="/courses">Courses</NavLink></li>
     </ul>
